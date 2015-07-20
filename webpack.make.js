32b318e5e3b07d0a45297f4ed2949af97f13b52b
Cesar Andreu <cesarandreu@gmail.com>
2015-07-20 02:07:11 -0700

    Fix babel loader configuration

--- webpack.make.js
+++ webpack.make.js
@@ -102,7 +102,7 @@ module.exports = function makeWebpackConfig (options) {
       // Transpile .js files using babel-loader
       // Compiles ES6 and ES7 into ES5 code
       test: /\.js$/,
-      loader: 'babel?optional=runtime',
+      loader: 'babel?optional[]=runtime',
       exclude: /node_modules/
     }, {
       // ASSET LOADER
@@ -135,7 +135,7 @@ module.exports = function makeWebpackConfig (options) {
   // Transpile .jsx files using babel-loader
   var jsxLoader = {
     test: /\.jsx$/,
-    loader: 'babel?optional=runtime',
+    loader: 'babel?optional[]=runtime',
     exclude: /node_modules/
   }
 
